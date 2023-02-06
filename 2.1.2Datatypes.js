f22d2239f7b7aa89f03dfbd9a6e068dd5efc577b
9ns2 <9ns2@fresnou.org>
2023-02-06 09:51:09 -0800

    initial commit

---
+++ 2.1.2Datatypes.js
@@ -0,0 +1,37 @@
+console.log(7);
+//This is a comment
+
+//Numbers
+//Integers
+console.log(-3);
+console.log(0);
+console.log(3);
+//Floating Point Number
+console.log(3.14);
+console.log(100.045);
+
+//Strings
+//Characters, letters, symbols, space
+console.log("Hello world");
+console.log("Hello world!!");
+console.log("Hello world 123");
+console.log('16,0');
+
+//Arithmetic Operators (Math)
+console.log(14+2);
+console.log(12-2);
+console.log(14+2); //shift-8 gives us *
+console.log(14/2);
+console.log(14%2); //% is called MOD and means remainder after dividing
+console.log(15%10);
+
+//Concatenation: combine strings
+console.log("Chuck Norris"+"is Ms Bolin.");
+console.log('My+'+ 'age'+'is''+ 15 +'+'.');
+console.log("your age is\n17.");
+
+//Template literal -plug in a calculated value
+//use backticks `` next to the 1
+console.log(`15 divided by 10=${15/10} axcvzxcvx`);
+
+
